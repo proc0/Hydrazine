9823fd227e03d3b77479e62eb8f8d536d1edb14c
proc0 <alanvx@gmail.com>
2025-10-21 06:34:55 -0700

    testing reverse engineering writes to the api

---
+++ script/login.js
@@ -0,0 +1,124 @@
+const convertRequestBodyToFormUrlEncoded = (data) => {
+  const bodyKeys = Object.keys(data)
+  const str = []
+  for (let i = 0; i < bodyKeys.length; i += 1) {
+    const thisKey = bodyKeys[i]
+    const thisValue = data[thisKey]
+    str.push(`${encodeURIComponent(thisKey)}=${encodeURIComponent(thisValue)}`)
+  }
+  return str.join('&')
+}
+
+class Login {
+  BaseURL = 'https://news.ycombinator.com'
+
+  logUser(username, password) {
+    let headers = new Headers({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Access-Control-Allow-Origin': '*',
+    })
+
+    return fetch(`${this.BaseURL}/login`, {
+      method: 'POST',
+      headers: headers,
+      body: convertRequestBodyToFormUrlEncoded({
+        acct: username,
+        pw: password,
+        goto: 'news',
+      }),
+      mode: 'no-cors',
+      credentials: 'include',
+    })
+      .then((res) => res.text())
+      .then((body) => {
+        console.log(body)
+        if (body.match(/Bad Login/i)) {
+          return false
+        } else {
+          return true
+        }
+      })
+  }
+
+  getUpvoteURL(id) {
+    let headers = new Headers({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Access-Control-Allow-Origin': '*',
+      'Cookie': 'user=proc0&Nwg5zsZJgTWqqYwoIjI9qchA7KaLZXeB',
+    })
+
+    return fetch(`${this.BaseURL}/item?id=${id}`, {
+      headers,
+      mode: 'no-cors',
+      credentials: 'include',
+    })
+      .then((res) => res.text())
+      .then((body) => {
+        // const doc = cheerio.load(body)
+        console.log(body)
+
+        // return doc(`#up_${id}`).attr('href')
+      })
+  }
+
+  getHmac(id) {
+    return fetch(`${this.BaseURL}/item?id=${id}`, {
+      mode: 'no-cors',
+      credentials: 'include',
+    })
+      .then((res) => res.text())
+      .then((body) => {
+        const doc = cheerio.load(body)
+
+        return doc('input[name=hmac]').attr('value')
+      })
+  }
+
+  upvote(id) {
+    return this.getUpvoteURL(id)
+      .then((url) =>
+        fetch(`${this.BaseURL}/${url}`, {
+          mode: 'no-cors',
+          credentials: 'include',
+        })
+      )
+      .then((res) => res.text())
+      .then((body) => {
+        return true
+      })
+      .catch((error) => {
+        console.log(error)
+        return false
+      })
+  }
+
+  reply(id, text) {
+    return this.getHmac(id)
+      .then((hmac) => {
+        let headers = new Headers({
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Access-Control-Allow-Origin': '*',
+        })
+
+        return fetch(`${this.BaseURL}/comment`, {
+          method: 'POST',
+          headers: headers,
+          body: convertRequestBodyToFormUrlEncoded({
+            parent: id,
+            goto: `item?id=${id}`,
+            hmac: hmac,
+            text: text,
+          }),
+          mode: 'no-cors',
+          credentials: 'include',
+        })
+      })
+      .then((res) => res.text())
+      .then((body) => {
+        return {
+          success: true,
+          error: null,
+        }
+      })
+  }
+}
--- script/main.js
+++ script/main.js
@@ -28,36 +28,9 @@ window.onload = function () {
     .catch((error) => {
       console.error('There was a problem with the fetch operation:', error)
     })
-}
 
-function processFetch() {
-  const request = fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-      }
-      return response.json() // Or .text(), .blob(), etc., depending on the response type
-    })
-    .then((data) => {
-      console.log(data) // Handle the fetched data here
-      const ul = document.createElement('ul')
-      document.querySelector('main').appendChild(ul)
-      for (let i = 0; i < 5; i++) {
-        const post = fetch(`https://hacker-news.firebaseio.com/v0/item/${data[i]}.json`)
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`)
-            }
-            return response.json() // Or .text(), .blob(), etc., depending on the response type
-          })
-          .then((data) => {
-            const li = document.createElement('li')
-            li.textContent = data.title
-            ul.appendChild(li)
-          })
-      }
-    })
-    .catch((error) => {
-      console.error('There was a problem with the fetch operation:', error)
-    })
+  const login = new Login()
+
+  login.logUser('proc0', '')
+  login.getUpvoteURL('45653143')
 }
